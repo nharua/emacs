cf4a3e011acca862e0d2d0acd57f562d5afc2b3c
Huynh Van Vinh <vinhhv@live.com>
2024-04-01 16:42:20 +0700

    Config v2

--- init.el
+++ init.el
@@ -1,13 +1,6 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
+;; Package manager
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 ;; Install and configure packages
@@ -15,10 +8,42 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(yasnippet verilog-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 123 :width normal))))
+;; '(font-lock-builtin-face ((t (:foreground "LightCoral"))))
+;; '(font-lock-comment-delimiter-face ((t (:foreground "#969896"))))
+;; '(font-lock-comment-face ((t (:foreground "green"))))
+;; '(font-lock-constant-face ((t (:foreground "DarkOliveGreen3"))))
+;; '(font-lock-function-name-face ((t (:foreground "red"))))
+;; '(font-lock-keyword-face ((t (:foreground "tomato"))))
+;; '(font-lock-negation-char-face ((t (:foreground "DeepSkyBlue"))))
+;; '(font-lock-preprocessor-face ((t (:foreground "gold"))))
+;; '(font-lock-regexp-grouping-backslash ((t (:foreground "#c397d8" :weight bold))))
+;; '(font-lock-type-face ((t (:foreground "deep sky blue")))))
+ )
 
-(setq save-place-file "~/.emacs.d/saveplace")
-(setq-default save-place t)
-(require 'saveplace)
+;; Load verilog mode only when needed
+(autoload 'verilog-mode "verilog-mode" "Verilog mode" t )
+;; Any files that end in .v, .dv or .sv should be in verilog mode
+(add-to-list 'auto-mode-alist '("\\.[ds]?va?h?\\'" . verilog-mode))
+(add-hook 'verilog-mode-hook '(lambda () (font-lock-mode 1)))
+
+;; Insert header
+(use-package yasnippet
+  :ensure t
+  :config
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+  (yas-global-mode 1))
 
 ;; Write backups to ~/.emacs.d/backup/
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
@@ -36,50 +61,13 @@
 (setq-default cursor-type 'bar)
 (setq-default truncate-lines t)
 (when window-system (set-frame-size (selected-frame) 160 48))
-(add-hook 'prog-mode-hook 'linum-mode)
 (add-hook 'before-save-hook 'my-prog-nuke-trailing-whitespace)
 (defun my-prog-nuke-trailing-whitespace ()
   (when (derived-mode-p 'prog-mode)
     (delete-trailing-whitespace)))
 (setq-default show-trailing-whitespace t)
-
-;; Set custom interface
-;; (custom-set-faces
-;; ;; custom-set-faces was added by Custom.
-;; ;; If you edit it by hand, you could mess it up, so be careful.
-;; ;; Your init file should contain only one such instance.
-;; ;; If there is more than one, they won't work right.
-;; '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 123 :width normal))))
-;; '(font-lock-builtin-face ((t (:foreground "LightCoral"))))
-;; '(font-lock-comment-delimiter-face ((t (:foreground "#969896"))))
-;; '(font-lock-comment-face ((t (:foreground "green"))))
-;; '(font-lock-constant-face ((t (:foreground "DarkOliveGreen3"))))
-;; '(font-lock-function-name-face ((t (:foreground "red"))))
-;; '(font-lock-keyword-face ((t (:foreground "tomato"))))
-;; '(font-lock-negation-char-face ((t (:foreground "DeepSkyBlue"))))
-;; '(font-lock-preprocessor-face ((t (:foreground "gold"))))
-;; '(font-lock-regexp-grouping-backslash ((t (:foreground "#c397d8" :weight bold))))
-;; '(font-lock-type-face ((t (:foreground "deep sky blue")))))
-
-;; Verilog/SystemVerilog mode
-;; Ref: https://www.verilog.com/emacs_install.html
-(defun prepend-path ( my-path )
-(setq load-path (cons (expand-file-name my-path) load-path)))
-
-(defun append-path ( my-path )
-(setq load-path (append load-path (list (expand-file-name my-path)))))
-;; Look first in the directory ~/elisp for elisp files
-(prepend-path "~/.emacs.d/elisp")
-;; Load verilog mode only when needed
-(autoload 'verilog-mode "verilog-mode" "Verilog mode" t )
-;; Any files that end in .v, .dv or .sv should be in verilog mode
-(add-to-list 'auto-mode-alist '("\\.[ds]?v\\'" . verilog-mode))
-;; Any files in verilog mode should have their keywords colorized
-(add-hook 'verilog-mode-hook '(lambda () (font-lock-mode 1)))
-
-;; Insert header
-(use-package yasnippet
-  :ensure t
-  :config
-  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-  (yas-global-mode 1))
+;; (add-hook 'prog-mode-hook 'linum-mode) ; This function is considered obsolete as of version 29.1 (Read more https://www.emacswiki.org/emacs/LinumMode)
+;; Change to this from Emacs 29.x version to show line number
+;; Preset `nlinum-format' for minimum width.
+;; Alternatively, to use it only in programming modes:
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
