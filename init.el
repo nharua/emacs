8bcdfe04530b08f5d3c5efbc06050f78c6b90e63
Huynh Van Vinh <vinhhv@live.com>
2024-07-01 17:22:58 +0700

    System Scrafters Version

--- init.el
+++ init.el
@@ -1,73 +1,181 @@
-;; Package manager
-(require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
+;; Start-up options
+;; (setq warning-minimum-level :emergency)
+(setq frame-title-format "%b")
+(setq inhibit-startup-screen t)
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
-;; Install and configure packages
+(scroll-bar-mode -1)    ; Disable visible scrollbar
+(tool-bar-mode -1)      ; Disable the toolbar
+(tooltip-mode -1)       ; Disable tooltips
+(set-fringe-mode 10)    ; Give some breathing room
+(menu-bar-mode -1)      ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Set font and font size
+;; M-x describe-font to list all available font
+(set-face-attribute 'default nil :font "JetBrainsMonoNL Nerd Font Mono" :height 120)
+
+;; Load dark theme
+(load-theme 'wombat)
+
+;; Global Key Binding
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Show line number
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(setq display-line-numbers 'relative)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Initialize package sources
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(setq package-archive-priorities '(("melpa"  . 100)
+                                   ("gnu"    .  50)
+                                   ("nongnu" .  25)))
+
+;; Ensure that use-package is installed.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(require 'use-package)
+
+(use-package use-package
+  :init
+  (setq use-package-always-ensure t)
+  (use-package use-package-ensure-system-package
+    :ensure t))
+
+(use-package counsel
+  :ensure t
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history)
+	 )
+  )
+
+;; Advance search mode
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)		
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; (add-to-list 'load-path "~/.emacs.d/elpa/compat-29.1.4.5")
+;; (require 'compat)
+
+;; Advance GUI Mode
+;; NOTE: The first time you load your configuration on a new machine,
+;; you need to run the following command interactively sothat mode line
+;; icons display correctly
+;;
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
+
+;; Install status bar
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+;; Load more theme
+(use-package doom-themes
+  :init (load-theme 'doom-gruvbox t))
+
+;; Rainbow delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; A better Emacs *help* buffer
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; Text Scale
+(use-package hydra)
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+;; Project management and navigation
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/workDir/local")
+    (setq projectile-project-search-path '("~/workDir/local")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+;; LSP Mode
+(use-package eldoc)
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t)
+  )
+
+;(rune/leader-keys
+;  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(yasnippet verilog-mode)))
+ '(package-selected-packages
+   '(gnu-elpa-keyring-update spinner lsp-mode counsel-projectile projectile hydra all-the-icons cmake-mode which-key use-package-ensure-system-package rainbow-delimiters ivy-rich helpful doom-themes doom-modeline counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 123 :width normal))))
-;; '(font-lock-builtin-face ((t (:foreground "LightCoral"))))
-;; '(font-lock-comment-delimiter-face ((t (:foreground "#969896"))))
-;; '(font-lock-comment-face ((t (:foreground "green"))))
-;; '(font-lock-constant-face ((t (:foreground "DarkOliveGreen3"))))
-;; '(font-lock-function-name-face ((t (:foreground "red"))))
-;; '(font-lock-keyword-face ((t (:foreground "tomato"))))
-;; '(font-lock-negation-char-face ((t (:foreground "DeepSkyBlue"))))
-;; '(font-lock-preprocessor-face ((t (:foreground "gold"))))
-;; '(font-lock-regexp-grouping-backslash ((t (:foreground "#c397d8" :weight bold))))
-;; '(font-lock-type-face ((t (:foreground "deep sky blue")))))
- )
-
-;; Load verilog mode only when needed
-(autoload 'verilog-mode "verilog-mode" "Verilog mode" t )
-;; Any files that end in .v, .dv or .sv should be in verilog mode
-(add-to-list 'auto-mode-alist '("\\.[ds]?va?h?\\'" . verilog-mode))
-(add-hook 'verilog-mode-hook '(lambda () (font-lock-mode 1)))
-
-;; Insert header
-(use-package yasnippet
-  :ensure t
-  :config
-  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-  (yas-global-mode 1))
-
-;; Write backups to ~/.emacs.d/backup/
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying      t  ; Don't de-link hard links
-      version-control        t  ; Use version numbers on backups
-      delete-old-versions    t  ; Automatically delete excess backups:
-      kept-new-versions      20 ; how many of the newest versions to keep
-      kept-old-versions      5) ; and how many of the old
-
-;; Start-up options
-(setq warning-minimum-level :emergency)
-(setq frame-title-format "%b")
-(setq inhibit-startup-screen t)
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-(setq-default cursor-type 'bar)
-(setq-default truncate-lines t)
-(when window-system (set-frame-size (selected-frame) 160 48))
-(add-hook 'before-save-hook 'my-prog-nuke-trailing-whitespace)
-(defun my-prog-nuke-trailing-whitespace ()
-  (when (derived-mode-p 'prog-mode)
-    (delete-trailing-whitespace)))
-(setq-default show-trailing-whitespace t)
-;; (add-hook 'prog-mode-hook 'linum-mode) ; This function is considered obsolete as of version 29.1 (Read more https://www.emacswiki.org/emacs/LinumMode)
-;; Change to this from Emacs 29.x version to show line number
-;; Preset `nlinum-format' for minimum width.
-;; Alternatively, to use it only in programming modes:
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
